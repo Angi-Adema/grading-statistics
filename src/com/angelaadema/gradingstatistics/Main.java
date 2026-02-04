fd18ea6ed9ffa7b633ceeb69284ef2b1ac8f47bf
Angi-Adema <dr.adema@gmail.com>
2026-02-04 14:17:23 -0700

    added project pseudocode

--- src/com/angelaadema/gradingstatistics/Main.java
+++ src/com/angelaadema/gradingstatistics/Main.java
@@ -1,9 +1,52 @@
 package com.angelaadema.gradingstatistics;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// PSEUDOCODE
+		
+		// START
+		//	   DECLARATIONS
+		//			enteredGrade
+		//			sum
+		// 			average
+		//			maximum
+		//			minimum
+		//          PROMPT_GRADE
+		//
+		//     SET PROMPT_GRADE TO "Enter a grade or enter 0 if finished: "
+		//
+		//	   CREATE Scanner object FOR user input
+		//
+		//     FOR i = 0 through 9
+		//          PRINT PROMPT_GRADE
+		//          READ enteredGrade
+		//          ADD grade to sum
+		//		    
+		//          IF i == 0
+		//				SET maximum to enteredGrade
+		//              SET minimum to enteredGrade
+		//			ELSE
+		//              IF enteredGrade > maximum THEN
+		//                  SET maximum to enteredGrade
+		//              ENDIF
+		//          
+		//			    IF enteredGrade < minimum THEN
+		//				    SET minimum to enteredGrade
+		//			    ENDIF
+		//          ENDIF
+		//     ENDFOR
+		//
+		//     SET average = sum / 10
+		//
+		//     PRINT average
+		//     PRINT maximum
+		//     PRINT minimum
+		// END
+		
+		
 
 	}
 
